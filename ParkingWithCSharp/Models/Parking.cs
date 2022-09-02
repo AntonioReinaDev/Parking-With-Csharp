b29d73b7f71a14f40cd5f235fffaa1613855325a
Antonio Reina <antonioreinadev@hotmail.com>
2022-09-02 11:27:30 -0300

    Add project files.

---
+++ ParkingWithCSharp/Models/Parking.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkingWithCSharp.Models
+{
+    public class Parking
+    {
+        private readonly decimal InitialPrice = 0;
+        private readonly decimal PricePerHour = 0;
+        private readonly List<string> Vehicles = new List<string>();
+
+        public Parking(decimal initialPrice, decimal pricePerHour)
+        {
+            InitialPrice = initialPrice;
+            PricePerHour = pricePerHour;
+        }
+
+        public void AddVehicle()
+        {
+            Console.WriteLine("Enter vehicle license plate to park:");
+            Vehicles.Add(Console.ReadLine());
+        }
+
+        public void RemoveVehicle()
+        {
+            Console.WriteLine("Enter vehicle license plate to remove:");
+
+            string licensePlate;
+            licensePlate = Console.ReadLine();
+
+            if (Vehicles.Any(x => x.ToUpper() == licensePlate.ToUpper()))
+            {
+                int hours;
+                decimal totalValue;
+                Console.WriteLine("Enter the number of hours the vehicle was parked:");
+
+                hours = int.Parse(Console.ReadLine());
+                totalValue = InitialPrice + PricePerHour * hours;
+
+                Vehicles.Remove(licensePlate);
+
+                Console.WriteLine($"The vehicle {licensePlate} was removed and the total price was: R$ {totalValue}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that vehicle is not parked here. Check if you typed the card correctly");
+            }
+        }
+
+        public void ListVehicles()
+        {
+            if (Vehicles.Any())
+            {
+                Console.WriteLine("Parked vehicles are:");
+                Vehicles.ForEach(vehicle => Console.WriteLine(vehicle));
+            }
+            else
+            {
+                Console.WriteLine("There are no parked vehicles.");
+            }
+        }
+    }
+}
---
+++ ParkingWithCSharp/Program.cs
@@ -0,0 +1,60 @@
+﻿using ParkingWithCSharp.Models;
+using System;
+
+namespace ParkingWithCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal initialPrice = 0;
+            decimal pricePerHour = 0;
+
+            Console.WriteLine("Welcome to the parking system!");
+            Console.WriteLine("Enter the starting price: ");
+            initialPrice = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Now enter the hourly price:");
+            pricePerHour = Convert.ToDecimal(Console.ReadLine());
+
+            Parking parking = new Parking(initialPrice, pricePerHour);
+
+            string option = string.Empty;
+            bool displayMenu = true;
+
+            while (displayMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter your option:");
+                Console.WriteLine("1 - Register vehicle");
+                Console.WriteLine("2 - Remove vehicle");
+                Console.WriteLine("3 - List vehicles");
+                Console.WriteLine("4 - Close");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        parking.AddVehicle();
+                        break;
+                    case "2":
+                        parking.RemoveVehicle();
+                        break;
+                    case "3":
+                        parking.ListVehicles();
+                        break;
+                    case "4":
+                        displayMenu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        break;
+                }
+
+                Console.WriteLine("Press a key to continue.");
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("The program has closed.");
+        }
+    }
+}
